b6f45ec2d67d28cdcb4fb89cd6bd2ee14ae99f5c
NoizeDaemon <freidario@gmx.at>
2023-12-21 10:01:56 +0100

    Mars Lander - Episode 1 (WIP)
    Still need to hit 300 fuel left.

--- Puzzle.cs
+++ Puzzle.cs
@@ -7,7 +7,13 @@ using System.Collections.Generic;
 
 class Player
 {
-    const float g = 3.711f;
+    const float g = -3.711f;
+    const float vSpeedLimit = -40.0f;
+
+    public static float GetSpeed(float startingSpeed, float acceleration, float timeInSeconds = 1.0f)
+    {
+        return acceleration * timeInSeconds + startingSpeed;
+    }
 
     static void Main(string[] args)
     {
@@ -22,34 +28,29 @@ class Player
 
         IEnumerable<(int X, int Y)> surface = Enumerable.Range(0, surfaceN)
                                                         .Select(_ => Console.ReadLine().Split(' '))
-                                                        .Select(s => (int.Parse(s[0]), int.Parse(s[1])));
+                                                        .Select(s => (int.Parse(s[0]), int.Parse(s[1])))
+                                                        .ToArray();
 
         // game loop
         while (true)
         {
-            //inputs = Console.ReadLine().Split(' ');
-            //int X = int.Parse(inputs[0]);
-            //int Y = int.Parse(inputs[1]);
-            //int hSpeed = int.Parse(inputs[2]); // the horizontal speed (in m/s), can be negative.
-            //int vSpeed = int.Parse(inputs[3]); // the vertical speed (in m/s), can be negative.
-            //int fuel = int.Parse(inputs[4]); // the quantity of remaining fuel in liters.
-            //int rotate = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
-            //int power = int.Parse(inputs[6]); // the thrust power (0 to 4).
-
             var input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            (int X, int Y, int hSpeed, int vSpeed, int fuel, int rotate, int power) lander = (input[0], input[1], input[2], input[3], input[4], input[5], input[6]);
+            (int X, int Y, int hSpeed, int vSpeed, int fuel, int rotate, int power) = (input[0], input[1], input[2], input[3], input[4], input[5], input[6]);
 
-            int deltaH = lander.Y - surface.Single(p => p.X == lander.X).Y;
+            int minPower = Math.Max(power - 1, 0);
+            int maxPower = Math.Min(power + 1, 4);
 
+            Console.Error.WriteLine($"Min: {minPower}, Max: {maxPower}");
 
-            if ()
+            foreach(int p in Enumerable.Range(minPower, maxPower - minPower + 1)) Console.Error.WriteLine($"{p}, {Math.Abs(GetSpeed(vSpeed, g + (float)p) - vSpeedLimit)}");
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
+            var targetPower = vSpeed + g > vSpeedLimit ? 0 : Enumerable.Range(minPower, maxPower - minPower + 1)
+                                                                   .OrderBy(p => Math.Abs(GetSpeed(vSpeed, g + (float)p) - vSpeedLimit))
+                                                                   .First();
 
-
-            // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
-            Console.WriteLine("0 3");
+            targetPower = Math.Min(fuel, targetPower);
+            
+            Console.WriteLine($"0 {targetPower}");
         }
     }
 }
